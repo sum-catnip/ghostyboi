61dab423e80708a63f0ce202d2338a3c75722a8c
sum-catnip <catnip@catnip.fyi>
2022-12-11 00:14:40 +0100

    speech bubbles

--- init.gd
+++ init.gd
@@ -1,17 +1,11 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var native = preload("res://native.gdns").new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# TODO: cover multiple monitors
-	# TODO: exclude clickable entities in passthrough
-	# https://github.com/beeznutzdev/godot-click-through-transparent-window/blob/main/src/Scripts/Canvas.gd
-	
+	native._ready()
+
 	var ss: Vector2 = OS.get_screen_size()
 	OS.set_window_size(ss - Vector2(1, 1))
 	OS.set_window_position(Vector2(0, 0))
@@ -20,9 +14,17 @@ func _ready():
 	OS.set_borderless_window(true)
 	OS.set_window_per_pixel_transparency_enabled(true)
 	get_tree().get_root().set_transparent_background(true)
-	print_debug(get_viewport().size)
-	OS.set_window_mouse_passthrough([Vector2(0, 0), Vector2(0, 0)])
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	
+	var min_i = 0
+	var min_x = 0
+	var size = Vector2(0, 0)
+	for i in range(OS.get_screen_count()):
+		var pos_x = OS.get_screen_position(i).x
+		size += OS.get_screen_size(i)
+		if pos_x < min_x:
+			min_i = i
+			min_x = pos_x
+	
+	OS.set_current_screen(min_i)
+	OS.set_window_size(size)
+	native.window_clickthrough()
